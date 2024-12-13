b35a068064d8ff2ee62478fdf5cf492b0280b90c
Elysom <157110231+Elysom@users.noreply.github.com>
2024-12-13 12:16:25 +0100

    Update list.component.ts

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,25 +1,26 @@
 import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 import { OutletContext } from '@angular/router';
-
+//Decorador Component de los Characters
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  @Input()
+  //Decorador input para poner los character en este caso trunk
+@Input()
   public characterList: Character[] = [{
     name:'Trunk',
     power: 10
   }]
 
 //onDeleteId = Index value= number,
-
+//Evento para borrar un Character con el decorador output
 @Output()
 public onDelete: EventEmitter<number> = new EventEmitter();
 
-
+//Coje el numero del character y lo elimina de la lista
 onDeteteCharacter(index:number):void{
 
    this.onDelete.emit(index);
